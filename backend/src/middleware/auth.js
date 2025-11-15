a66df3f593caf01dbfb774e42b5cd8bfcf9908ad
ethio-man <natymiskir@gmail.com>
2025-11-15 09:04:39 -0300

    ownership based authorization

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -2,41 +2,37 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import prisma from "../startup/db.js";
 dotenv.config();
-export const authUser = async function (req, res, next) {
+export const auth = async function (req, res, next) {
   const token = req.header("auth-token");
   if (!token) return res.status(401).json("No token is provided");
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const { userId } = req.body;
-    const user = await prisma.users.findUnique({
-      where: { id: userId },
-    });
-    if (!user) return res.status(404).json("User not found.");
-    // so what shall i do ? create separate auth function for each action on separate table ,or one for all, how?
-    if (decoded.id === parseInt(req.params.id) || decoded.role === "admin") {
-      next();
-    } else {
-      res.status(403).json("Not authorized to make change in this case.");
-    }
+    req.user = decoded;
+    next();
   } catch (err) {
     res.status(400).json("Invalid token.", err);
   }
 };
-
-export function authAdmin(req, res, next) {
-  const token = req.header("auth-token");
-  if (!token) return res.status(401).json("No token is provided.");
+export const verifyOwnership = (model, ownerId) => async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (decoded.role === "admin") next();
-    else {
-      return res.status(403).json("unAuthorized");
+    const resource = await prisma[model].findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (!resource) return res.status(404).json(`${model} not found.`);
+    if ((req.user.role = "admin")) {
+      req.resource = resource;
+      return next();
     }
+    if (resource[ownerId] != req.params.id)
+      return res.status(403).json({ message: "Unauthorized access!" });
+    req.resource = resource;
+    next();
   } catch (err) {
-    res.status(403).json("Forbidden.", err);
+    console.log(err);
+    res.status(500).json({ message: `Server error.` });
   }
-}
-
+};
 export function authSuperAdmin(req, res, next) {
   const { username, password } = req.body;
   const superAdmin = JSON.parse(process.env.SUPER_ADMIN);
