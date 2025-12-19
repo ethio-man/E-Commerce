8cd7b869a92ddf203c0a18daa430950b457a9401
ethio-man <natymiskir@gmail.com>
2025-12-19 19:20:50 -0300

    schmea update to support sign in and sign up with google

--- backend/src/validations/userValidator.js
+++ backend/src/validations/userValidator.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 export const userSchema = Joi.object({
   full_name: Joi.string().min(2).max(53),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).max(100).required(),
+  password: Joi.string().min(6).max(100),
 });
