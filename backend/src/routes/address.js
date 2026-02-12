6632dfb7d4cdacf66b3fe41c3a71caf5bd51ae74
ethio-man <natymiskir@gmail.com>
2026-02-12 15:58:48 -0300

    input validation for address

--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { prisma } from "../startup/db.js";
 import { auth, verifyOwnership } from "../middleware/auth.js";
+import { addressSchema } from "../validations/addressValidation.js";
+import validate from "../middleware/validate.js";
 const route = express.Router();
 route.get("/", auth, async (req, res) => {
   if (req.user.role != "admin")
@@ -25,7 +27,7 @@ route.get("/:id", [auth, verifyOwnership("address")], async (req, res) => {
     res.status(404).json({ error: "Error to find address" });
   }
 });
-route.post("/", auth, async (req, res) => {
+route.post("/", [auth, validate(addressSchema)], async (req, res) => {
   const { user_id, country, city, postal_code } = req.body;
   try {
     const address = await prisma.address.create({
@@ -37,20 +39,29 @@ route.post("/", auth, async (req, res) => {
     res.status(404).json({ error: "Error to create address" });
   }
 });
-route.put("/:id", [auth, verifyOwnership("address")], async (req, res) => {
-  const { id } = req.params;
-  const { user_id, country, city, postal_code } = req.body;
-  try {
-    const address = await prisma.address.update({
-      where: { id: parseInt(id) },
-      data: { users: { connect: { id: user_id } }, country, city, postal_code },
-    });
-    res.json(address);
-  } catch (err) {
-    console.log(err);
-    res.status(404).json({ error: "Error to update address" });
-  }
-});
+route.put(
+  "/:id",
+  [auth, validate(addressSchema), verifyOwnership("address")],
+  async (req, res) => {
+    const { id } = req.params;
+    const { user_id, country, city, postal_code } = req.body;
+    try {
+      const address = await prisma.address.update({
+        where: { id: parseInt(id) },
+        data: {
+          users: { connect: { id: user_id } },
+          country,
+          city,
+          postal_code,
+        },
+      });
+      res.json(address);
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: "Error to update address" });
+    }
+  },
+);
 route.delete("/:id", [auth, verifyOwnership("address")], async (req, res) => {
   const { id } = req.params;
   try {
