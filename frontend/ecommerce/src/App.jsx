72fd5fed256394b8bb6f11db8800b5fe8d7b6591
ethio-man <natymiskir@gmail.com>
2026-02-18 14:04:40 -0300

    Routes to the admin pages is added

--- frontend/ecommerce/src/App.jsx
+++ frontend/ecommerce/src/App.jsx
@@ -7,11 +7,22 @@ import CheckOut from "./pages/CheckOut.jsx";
 import Login from "./pages/Login.jsx";
 import AddProduct from "./pages/admin/AddProduct.jsx";
 import OrderSummary from "./pages/OrderSummary.jsx";
+import { AdminAuthProvider } from "./context/AdminAuthContext.jsx";
+import AdminLayout from "./components/admin/AdminLayout.jsx";
+import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import AdminProducts from "./pages/admin/AdminProducts.jsx";
+import AdminOrders from "./pages/admin/AdminOrders.jsx";
+import AdminCustomers from "./pages/admin/AdminCustomers.jsx";
+import AdminReviews from "./pages/admin/AdminReviews.jsx";
+import AdminPayments from "./pages/admin/AdminPayments.jsx";
+import AdminAdministration from "./pages/admin/AdminAdministration.jsx";
 import "./App.css";
+
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Customer-facing routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/productList/:category" element={<ProductList />} />
         <Route path="/productOverview" element={<ProductOverview />} />
@@ -20,6 +31,25 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin/addProducts" element={<AddProduct />} />
         <Route path="/orderSummary" element={<OrderSummary />} />
+
+        {/* Admin panel routes */}
+        <Route
+          path="/admin"
+          element={
+            <AdminAuthProvider>
+              <AdminLayout />
+            </AdminAuthProvider>
+          }
+        >
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products/manage" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="customers" element={<AdminCustomers />} />
+          <Route path="reviews" element={<AdminReviews />} />
+          <Route path="payments" element={<AdminPayments />} />
+          <Route path="administration" element={<AdminAdministration />} />
+        </Route>
       </Routes>
     </Router>
   );
