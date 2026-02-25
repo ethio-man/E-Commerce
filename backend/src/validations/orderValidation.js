945237ac77af02da9cae7669d06a68307071a42d
ethio-man <natymiskir@gmail.com>
2026-02-25 07:50:56 -0300

    improvement on orders validation

--- backend/src/validations/orderValidation.js
+++ backend/src/validations/orderValidation.js
@@ -1,5 +1,7 @@
 import Joi from "joi";
 export const orderSchema = Joi.object({
+  name_on_bank: Joi.max(53).string().required(),
+  accountNo: Joi.max(53).string().required(),
   total_price: Joi.number().required(),
   payment_method: Joi.string().max(200).required(),
   user_id: Joi.number().required(),
